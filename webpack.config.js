ec6dc0ce15013b86d16690b68fcf6f849ed2ad62
Дудник Павел <ne.pridumal.nik@outlook.com>
2020-11-23 20:29:03 +0700

    Базовая настройка и настройка плагинов

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,33 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
   mode: "development",
   entry: "./index.js",
   output: {
-    filename: "bundle.js",
+    filename: "bundle.[hash].js",
     path: path.resolve(__dirname, "dist"),
   },
+  resolve: {
+    extensions: [".js"],
+    alias: { "@": path.resolve(__dirname, "src") },
+    alias: { "@core": path.resolve(__dirname, "src/core") },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: "index.html",
+    }),
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, "src/favicon.ico"),
+        to: path.resolve(__dirname, "dist"),
+      },
+    ]),
+    new MiniCssExtractPlugin({ filename: "bundle.[hash].css" }),
+  ],
 };
