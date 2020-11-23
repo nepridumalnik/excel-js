6d5a72260b52e83fa12d075ade20b9f0f72ac7f5
Дудник Павел <ne.pridumal.nik@outlook.com>
2020-11-23 20:41:27 +0700

    Добавлены загрузчики CSS и SCSS, исправлен синтаксис на новый стиль

--- webpack.config.js
+++ webpack.config.js
@@ -22,12 +22,22 @@ module.exports = {
     new HTMLWebpackPlugin({
       template: "index.html",
     }),
-    new CopyPlugin([
-      {
-        from: path.resolve(__dirname, "src/favicon.ico"),
-        to: path.resolve(__dirname, "dist"),
-      },
-    ]),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/favicon.ico"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),
     new MiniCssExtractPlugin({ filename: "bundle.[hash].css" }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };
